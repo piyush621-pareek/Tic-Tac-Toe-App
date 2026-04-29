09284a35eb34549e16e417c46dab4e7e81452c8c
piyush621 -pareek <piyushpareek621@gmail.com>
2026-04-29 11:07:03 +0530

    Coded UC7: Computer Makes a Random Move (Easy Level)

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,48 @@
+import java.util.Random;
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+    
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
-        placeMove(0, 0, 'X');
-        System.out.println(board[0][0]);
+        computerMove();
+
+        printBoard(); 
     }
 
-    static void placeMove(int row, int col, char symbol) {
-   
-        board[row][col] = symbol;
+    static void computerMove() {
+        Random random = new Random();
+        boolean validMoveFound = false;
+
+        System.out.println("Computer is making a move...");
+
+        while (!validMoveFound) {
+
+            int slot = random.nextInt(9) + 1; 
+
+            int row = (slot - 1) / 3;
+            int col = (slot - 1) % 3;
+
+            if (board[row][col] == '-') {
+
+                board[row][col] = computerSymbol;
+                validMoveFound = true; 
+                System.out.println("Computer successfully chose slot: " + slot);
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
