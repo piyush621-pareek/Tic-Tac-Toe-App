f23ec83456d94eaa798216db67e1f56e65af4ddf
piyush621 -pareek <piyushpareek621@gmail.com>
2026-04-29 11:20:32 +0530

    Coded UC10: Detect Draw Condition

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,35 +4,27 @@ public class TicTacToe {
 
     public static void main(String[] args) {
 
-        board[0][0] = 'X';
-        board[0][1] = 'X';
-        board[0][2] = 'X';
-
-        System.out.println(hasWon('X')); 
+        initializeBoard(); 
+        
+        System.out.println(isDraw());
     }
 
-    static boolean hasWon(char symbol) {
-
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
-                return true;
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
+                    return false;
+                }
             }
         }
+        return true;
+    }
 
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol) {
-                return true;
+    static void initializeBoard() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                board[r][c] = '-';
             }
         }
-
-        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
-            return true;
-        }
-
-        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
-            return true;
-        }
-
-        return false;
     }
 }
