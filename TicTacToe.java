975cdf8efbcbd61786c8189a9c67c25fde079bea
piyush621 -pareek <piyushpareek621@gmail.com>
2026-04-29 11:16:38 +0530

    Coded UC9: Check Winning Condition

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,41 +1,38 @@
 public class TicTacToe {
 
-    static boolean isHumanTurn = true;
-    static boolean gameOver = false;
+    static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        System.out.println("Game Started!\n");
 
-        while (!gameOver) {
+        board[0][0] = 'X';
+        board[0][1] = 'X';
+        board[0][2] = 'X';
 
-            if (isHumanTurn) {
-                System.out.println("Human Player's turn...");
-
-            } else {
-                System.out.println("Computer's turn...");
-
-            }
-
-            checkWinOrDraw(); 
-
-            isHumanTurn = !isHumanTurn;
-            
-            System.out.println("-------------------------");
-        }
-
-        System.out.println("Game Over!");
+        System.out.println(hasWon('X')); 
     }
-    
-    static int turnsTaken = 0;
-	
-    public static void checkWinOrDraw() {
-        turnsTaken++;
-		
-        if (turnsTaken >= 5) {
-            System.out.println("Win or Draw detected!");
-            gameOver = true; 
-        } else {
-            System.out.println("Game continues...");
+
+    static boolean hasWon(char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+                return true;
+            }
         }
+
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol) {
+                return true;
+            }
+        }
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+            return true;
+        }
+
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
     }
 }
